15aed9231ad81e66c1690ab83cd069d54821d991
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2021-08-07 16:41:17 -0500

    Add demo project

---
+++ src/DemoApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using PiServer;
+using PiServer.Nodes.SenseHat;
+using System;
+using System.Threading;
+
+namespace DemoApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var loggerFactory = LoggerFactory.Create(configure => configure.AddConsole());
+      var logger = loggerFactory.CreateLogger<Program>();
+
+      ISenseHat senseHat = CreateSenseHat(loggerFactory.CreateLogger<ISenseHat>());
+
+      var piServer = new OpcUaPiServer(options =>
+      {
+        options.SenseHat = senseHat;
+        options.AutoAccept = true;
+        options.Logger = loggerFactory.CreateLogger<OpcUaPiServer>();
+      });
+
+      try
+      {
+        OpcUaPiServer.LaunchServer(piServer, "Resources/Configuration/PiServer.Config.xml").Wait();
+
+        ManualResetEvent quitEvent = new ManualResetEvent(false);
+        Console.CancelKeyPress += (sender, eArgs) =>
+        {
+          quitEvent.Set();
+          eArgs.Cancel = true;
+        };
+
+        logger.LogInformation("Running server...");
+        quitEvent.WaitOne(Timeout.Infinite);
+        logger.LogWarning("Stopping server...");
+      }
+      catch (Exception ex)
+      {
+        logger.LogError($"Launch failed: {ex.Message}");
+      }
+      finally
+      {
+        OpcUaPiServer.StopServer(piServer);
+      }
+    }
+
+    private static ISenseHat CreateSenseHat(ILogger<ISenseHat> logger)
+    {
+      return new SenseHatSimulated(logger);
+    }
+  }
+}
---
+++ src/DemoApp/SenseHatSimulated.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Logging;
+using PiServer.Nodes.SenseHat;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace DemoApp
+{
+  public class SenseHatSimulated : ISenseHat
+  {
+    private readonly Random rng = new Random();
+    private ILogger<ISenseHat> Logger { get; }
+
+    public SenseHatSimulated(ILogger<ISenseHat> logger)
+    {
+      Logger = logger;
+    }
+
+    private void Log(string msg)
+    {
+      Logger?.LogInformation(msg);
+    }
+
+    public int SensorUpdateRate => 500;
+
+    public string TemperatureUnits => "Celsius";
+    public double Temperature()
+    {
+      return 20.0 + rng.NextDouble() * 2;
+    }
+
+    public string PressureUnits => "Hectopascals";
+    public double Pressure()
+    {
+      return 1013.25 + (rng.NextDouble() - rng.NextDouble()) * 5;
+    }
+
+    public string HumidityUnits => "Relative humidity";
+    public double Humidity()
+    {
+      return 60 + (rng.NextDouble() - rng.NextDouble()) * 10;
+    }
+
+    public string MagnetometerUnits => "Gauss";
+    public Vector3 Magnetometer()
+    {
+      float x = (float)(rng.NextDouble()-rng.NextDouble());
+      float y = (float)(rng.NextDouble()-rng.NextDouble());
+      float z = (float)(rng.NextDouble()-rng.NextDouble());
+
+      return new Vector3(x, y, z);
+    }
+
+    public string AccelerometerUnits => "G";
+    public Vector3 Accelerometer()
+    {
+      float x = (float)(rng.NextDouble()-rng.NextDouble());
+      float y = (float)(rng.NextDouble()-rng.NextDouble());
+      float z = (float)(rng.NextDouble()-rng.NextDouble());
+
+      return new Vector3(x, y, z);
+    }
+
+    public string AngularRateUnits => "Degrees/second";
+    public Vector3 AngularRate()
+    {
+      float x = (float)(rng.NextDouble()-rng.NextDouble());
+      float y = (float)(rng.NextDouble()-rng.NextDouble());
+      float z = (float)(rng.NextDouble()-rng.NextDouble());
+
+      return new Vector3(x, y, z);
+    }
+
+    public bool JoystickDown()
+    {
+      return (rng.Next(0,2) == 1);
+    }
+
+    public bool JoystickLeft()
+    {
+      return (rng.Next(0,2) == 1);
+    }
+
+    public bool JoystickPushbutton()
+    {
+      return (rng.Next(0,2) == 1);
+    }
+
+    public bool JoystickRight()
+    {
+      return (rng.Next(0,2) == 1);
+    }
+
+    public bool JoystickUp()
+    {
+      return (rng.Next(0,2) == 1);
+    }
+
+    public void SetLED(byte red, byte green, byte blue)
+    {
+      Log($"LED: {red}R {green}G {blue}B");
+    }
+  }
+}
