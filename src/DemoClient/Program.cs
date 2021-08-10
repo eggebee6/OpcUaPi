105cb8378568f53138e31bbb3ee7bd3a04b0f922
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2021-08-10 00:42:21 -0500

    Add DemoClient project

---
+++ src/DemoClient/Program.cs
@@ -0,0 +1,102 @@
+﻿using CommandLine;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using PiClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DemoClient
+{
+  class CommandLineOptions
+  {
+    [Option(shortName: 'u', longName: "endpointUrl", Required = true, HelpText = "Server endpoint Url")]
+    public string EndpointUrl { get; set; }
+  }
+  // Example:  DemoClient --endpointUrl opc.tcp://192.168.0.200:43101
+
+  class Program
+  {
+    private static ILogger<Program> logger = NullLogger<Program>.Instance;
+
+    private const int ParseOK = 0;
+    private const int ParseFailed = 1;
+
+    static void Main(string[] args)
+    {
+      string endpointUrl = null;
+
+      int result = Parser.Default
+        .ParseArguments<CommandLineOptions>(args)
+        .MapResult(
+          options =>
+          {
+            endpointUrl = options.EndpointUrl;
+            return ParseOK;
+          },
+          errors =>
+          {
+            return ParseFailed;
+          });
+
+      if (result != ParseOK)
+      {
+        Environment.Exit(result);
+      }
+
+      var loggerFactory = LoggerFactory.Create(configure => configure.AddConsole());
+      logger = loggerFactory.CreateLogger<Program>();
+
+      var client = new OpcUaPiClient(options =>
+      {
+        options.EndpointUrl = endpointUrl;
+        options.AutoAccept = true;
+        options.Logger = loggerFactory.CreateLogger<OpcUaPiClient>();
+      });
+
+      SenseHatClient senseHat;
+      Timer updateTimer = null;
+
+      try
+      {
+        OpcUaPiClient.LaunchClient(client, "Resources/Configuration/PiClient.Config.xml").Wait();
+
+        senseHat = new SenseHatClient(client, loggerFactory.CreateLogger<SenseHatClient>());
+        senseHat.Initialize();
+
+        updateTimer = new Timer(_ =>
+        {
+          StringBuilder builder = new StringBuilder();
+          builder.AppendLine($"Temperature: {senseHat.Temperature}");
+          builder.AppendLine($"Pressure: {senseHat.Pressure}");
+          builder.AppendLine($"Humidity: {senseHat.Humidity}");
+
+          logger.LogInformation(builder.ToString());
+        }, null, 1000, 5000);
+
+        ManualResetEvent quitEvent = new ManualResetEvent(false);
+        Console.CancelKeyPress += (sender, eArgs) =>
+        {
+          quitEvent.Set();
+          eArgs.Cancel = true;
+        };
+
+        logger.LogInformation("Running client...");
+        quitEvent.WaitOne(Timeout.Infinite);
+        logger.LogWarning("Stopping client...");
+      }
+      catch (Exception ex)
+      {
+        logger.LogError($"Launch failed: {ex.Message}");
+      }
+      finally
+      {
+        OpcUaPiClient.StopClient(client);
+
+        updateTimer?.Dispose();
+        updateTimer = null;
+      }
+    }
+  }
+}
---
+++ src/DemoClient/SenseHatClient.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.Extensions.Logging;
+using Opc.Ua;
+using Opc.Ua.Client;
+using PiClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoClient
+{
+  public class SenseHatClient
+  {
+    private readonly OpcUaPiClient client;
+    private readonly ILogger<SenseHatClient> logger;
+
+    public const string TemperatureSensorNode = "/SenseHat/Temperature sensor/Output";
+    public const string PressureSensorNode = "/SenseHat/Pressure sensor/Output";
+    public const string HumiditySensorNode = "/SenseHat/Humidity sensor/Output";
+
+    private readonly List<string> nodePaths = new List<string>()
+    {
+      TemperatureSensorNode,
+      PressureSensorNode,
+      HumiditySensorNode
+    };
+    private readonly Dictionary<string, NodeId> nodeMap = new Dictionary<string, NodeId>();
+
+    public double Temperature { get; private set; }
+    public double Pressure { get; private set; }
+    public double Humidity { get; private set; }
+
+    private NodeId piFolder;
+
+    Subscription subscription;
+
+    public SenseHatClient(OpcUaPiClient client, ILogger<SenseHatClient> logger)
+    {
+      if (client is null)
+      {
+        throw new ArgumentNullException(nameof(client));
+      }
+
+      this.client = client;
+      this.logger = logger;
+    }
+
+    private void Log(string msg, LogLevel level = LogLevel.Information)
+    {
+      logger?.Log(level, msg);
+    }
+
+    public void Initialize()
+    {
+      if (client.Session is null)
+      {
+        throw new ArgumentException("Client session is null");
+      }
+
+      piFolder = FindPiFolder() ?? throw new Exception("Failed to find Pi folder");
+
+      FindNodeIdsByBrowseName(piFolder);
+      ConfigureSubscriptions();
+    }
+
+    private void ConfigureSubscriptions()
+    {
+      subscription = new Subscription()
+      {
+        PublishingEnabled = true,
+        PublishingInterval = 500
+      };
+
+      foreach (var key in nodeMap.Keys)
+      {
+        NodeId id = nodeMap[key];
+        if (id is null)
+        {
+          continue;
+        }
+
+        var monitor = new MonitoredItem()
+        {
+          MonitoringMode = MonitoringMode.Reporting,
+          StartNodeId = id,
+          AttributeId = Attributes.Value
+        };
+
+        MonitoredItemNotificationEventHandler handler = key switch
+        {
+          TemperatureSensorNode => OnTemperatureChanged,
+          PressureSensorNode => OnPressureChanged,
+          HumiditySensorNode => OnHumidityChanged,
+          _ => OnUnhandledChange
+        };
+        monitor.Notification += new MonitoredItemNotificationEventHandler(handler);
+
+        subscription.AddItem(monitor);
+      }
+
+      client.Session.AddSubscription(subscription);
+      subscription.Create();
+    }
+
+    private NodeId FindPiFolder()
+    {
+      client.Session.Browse(
+        null,
+        null,
+        ObjectIds.RootFolder,
+        0,
+        BrowseDirection.Forward,
+        ReferenceTypeIds.HierarchicalReferences,
+        true,
+        (uint)(NodeClass.Object),
+        out Byte[] continuationPoint,
+        out ReferenceDescriptionCollection references);
+
+      foreach (var reference in references)
+      {
+        if (reference.BrowseName.Name == "Pi")
+        {
+          return ExpandedNodeId.ToNodeId(reference.NodeId, client.Session.NamespaceUris);
+        }
+      }
+
+      return null;
+    }
+
+    private void FindNodeIdsByBrowseName(NodeId parentId, string browsePath = "")
+    {
+      client.Session.Browse(
+        null,
+        null,
+        ExpandedNodeId.ToNodeId(parentId, client.Session.NamespaceUris),
+        0,
+        BrowseDirection.Forward,
+        ReferenceTypeIds.HierarchicalReferences,
+        true,
+        (uint)(NodeClass.Variable | NodeClass.Object | NodeClass.Method),
+        out Byte[] continuationPoint,
+        out ReferenceDescriptionCollection references);
+
+      foreach (var reference in references)
+      {
+        var browseName = $"{browsePath}/{reference.BrowseName.Name}";
+        if (nodePaths.Contains(browseName))
+        {
+          nodeMap.Add(browseName, ExpandedNodeId.ToNodeId(reference.NodeId, client.Session.NamespaceUris));
+        }
+        else
+        {
+          FindNodeIdsByBrowseName(ExpandedNodeId.ToNodeId(reference.NodeId, client.Session.NamespaceUris), browseName);
+        }
+      }
+    }
+
+    private bool TryGetItemValue(MonitoredItem item, out double result)
+    {
+      result = default;
+
+      foreach (var val in item.DequeueValues())
+      {
+        var obj = val.Value as double?;
+        if (obj.HasValue)
+        {
+          result = obj.Value;
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    private void OnUnhandledChange(MonitoredItem item, MonitoredItemNotificationEventArgs evt)
+    {
+      return;
+    }
+
+    private void OnTemperatureChanged(MonitoredItem item, MonitoredItemNotificationEventArgs evt)
+    {
+      if (TryGetItemValue(item, out double res))
+      {
+        Temperature = res;
+      }
+    }
+
+    private void OnPressureChanged(MonitoredItem item, MonitoredItemNotificationEventArgs evt)
+    {
+      if (TryGetItemValue(item, out double res))
+      {
+        Pressure = res;
+      }
+    }
+
+    private void OnHumidityChanged(MonitoredItem item, MonitoredItemNotificationEventArgs evt)
+    {
+      if (TryGetItemValue(item, out double res))
+      {
+        Humidity = res;
+      }
+    }
+  }
+}
